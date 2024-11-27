2eb35d2b3a6798ffe70eaf4cf30b81c69a92c531
J0hann3 <jvigny@student.42.fr>
2024-11-27 14:56:31 +0100

    doc: add doc to functions in asm + fix strcpy

--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -1,27 +1,23 @@
 section .text
 global ft_strcpy
 
-ft_strcpy:
-	mov rax, rdi
-	cmp byte [rdi], 0
-	je .end
-	.loop:
-		mov byte dl, [rsi]
-		mov byte [rdi], dl
-		add rsi, 1
-		add rdi, 1
-		cmp byte [rdi], 0
-		jne .loop
-	.end:
-		ret
+; ft_strcpy copy the source into th destination
+; return pointer to the destination
+; char *ft_strcpy(char *dest, const char *src)
+; rsi is the source
+; rdi is the destination
 
-; ft_strcpy:
-; 	mov rax, rdi
-; 	cmp byte rdi, 0
-; 	je .end
-; 	.loop:
-; 		rep movsb rdi, rsi
-; 		cmp byte rdi, 0
-; 		jne .loop
-; 	.end:
-; 		ret
+ft_strcpy:
+	mov rax, rdi			; save destination pointer for return value
+	cmp byte [rsi], 0		; check if source if empty before copying bytes
+	je .end					; jump to the end if ZF is set to 1 => comp is equal
+	.loop:
+		mov byte dl, [rsi]	; save src char into register
+		mov byte [rdi], dl	; copy register into dest
+		add rsi, 1			; increment src pointer
+		add rdi, 1			; increment dest pointer
+		cmp byte [rsi], 0	; check if src end with \0
+		jne .loop			; jump to loop if comp is not equal to \0
+	.end:
+		mov byte [rdi], 0	; add \0 at the end of dest
+		ret					; return from the function
--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -1,14 +1,19 @@
-section .text
-global ft_strlen
+section .text					; section to write code
+global ft_strlen				; define function to be use outside of this file
+
+; ft_strlen return the length of the argument
+; return size_t
+; size_t ft_strlen(const char *s)
+; rdi is the string, we need to get its length
 
 ft_strlen:
-	mov rcx, 0
-	cmp byte rdi[rcx], 0
-	je .end
+	mov rcx, 0					; init counter to 0
+	cmp byte rdi[rcx], 0		; check if string is empty
+	je .end						; jump to the end if source is empty
 	.loop:
-		add rcx, 1
-		cmp byte rdi[rcx], 0
-		jne .loop
+		add rcx, 1				; increment counter by 1
+		cmp byte rdi[rcx], 0	; compare char with \0 to known if the string is ended
+		jne .loop				; if not jump to loop again
 	.end:
-		mov rax, rcx
-		ret
+		mov rax, rcx			; copy counter value to return value
+		ret						; return from the function
